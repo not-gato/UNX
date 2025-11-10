6e8129d36a82f97709530f75bc94c030b403ed1c
Gato <vitorkauanrandow9@gmail.com>
2025-11-10 19:46:15 -0400

    Update Flick.lua

--- Games/Flick.lua
+++ Games/Flick.lua
@@ -9,11 +9,11 @@ Library.ForceCheckbox = true
 Library.ShowToggleFrameInKeybinds = true
 
 local Window = Library:CreateWindow({
-	Title = "UNXHub",
-	Footer = "Version: " .. (getgenv().unxshared and getgenv().unxshared.version or "Unknown") .. ", Game: " .. (getgenv().unxshared and getgenv().unxshared.gamename or "Unknown"),
-	Icon = 123333102279908,
-	NotifySide = "Right",
-	ShowCustomCursor = true,
+    Title = "UNXHub",
+    Footer = "Version: " .. (getgenv().unxshared and getgenv().unxshared.version or "Unknown") .. ", Game: " .. (getgenv().unxshared and getgenv().unxshared.gamename or "Unknown") .. ", Player: " .. (getgenv().unxshared and getgenv().unxshared.playername or "Unknown"),
+    Icon = 123333102279908,
+    NotifySide = "Right",
+    ShowCustomCursor = true,
 })
 
 local Tabs = {
